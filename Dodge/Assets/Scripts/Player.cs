9fd3ab0b24684b1a86110f4869c93e739d3147a4
kimny22 <52498884+kimny22@users.noreply.github.com>
2019-07-06 23:04:36 +0900

    dodgeGame
    dodge falling boxes

---
+++ Dodge/Assets/Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    Vector3 character_pos;
+    Vector3 pos;
+    public Text timeSet;
+    public Text plusPoint;
+    public Canvas endGame;
+    bool isPause = false;
+    bool effect = false;
+    float nowTime;
+    float plusTime;
+    // Use this for initialization
+    void Start()
+    {
+        endGame.enabled = false;
+        nowTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSet.text = ((int)(Time.time-nowTime)).ToString();
+        if (Input.GetMouseButton(0) && endGame.enabled == false)
+        {
+            pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z-Camera.main.transform.position.z));
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (isPause)
+            {
+                Time.timeScale = 1;
+                isPause = false;
+            }
+            else
+            {
+                Time.timeScale = 0;
+                isPause = true;
+            }
+        }
+        character_pos.x = Mathf.Lerp(character_pos.x, pos.x, 0.1f);
+        transform.position = new Vector3(character_pos.x, transform.position.y, transform.position.z);
+        if(effect == true)
+        {
+            plusPoint.text = "+10";
+            plusTime = Time.time;
+            nowTime -= 10;
+            effect = false;
+        }
+        if (Time.time - plusTime > 1) plusPoint.text = "";
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Box")
+        {
+            Debug.Log("hit box");
+            Renderer Color = other.GetComponent<Renderer>();
+            Color.material.color = new Color(1f, 0f, 0f);
+            Time.timeScale = 0;
+            endGame.enabled = true;
+        }
+        if(other.tag == "item")
+        {
+            Debug.Log("get item");
+            Destroy(other.gameObject);
+            effect = true;
+        }
+    }
+    public void restart()
+    {
+        endGame.enabled = false;
+        Time.timeScale = 1;
+        nowTime = Time.time;
+    }
+}
---
+++ Dodge/Assets/Scripts/boxController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxController : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject boxObject;
+    public GameObject item1;
+    float nowTime;
+
+	void Start () {
+        nowTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time - nowTime > 0.5f)
+        {
+            GameObject part = Instantiate(boxObject, new Vector3(0f, 20f, 0f), Quaternion.identity); // 객체 생성
+            part.GetComponent<boxMove>().setPostion(Random.Range(-3, 3), 4f, Random.Range(0.05f, 0.1f));
+            nowTime = Time.time;
+            int i = Random.Range(1, 20);
+            if(i == 15)
+            {
+                GameObject item = Instantiate(item1, new Vector3(0f, 20f, 0f), Quaternion.identity); // 객체 생성
+                item.GetComponent<boxMove>().setPostion(Random.Range(-3, 3), 4f, Random.Range(0.05f, 0.1f));
+            }
+
+        }
+    }
+}
